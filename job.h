239ea07e349ef3fe5863acb26fcf183cc1d7e117
drortif <drort@campus.technion.ac.il>
2024-06-29 23:55:09 +0300

    saturday last update - decorations

---
+++ job.h
@@ -0,0 +1,13 @@
+#ifndef _JOB_H
+#define _JOB_H
+#include <unistd.h> 
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <iostream>
+#include <unistd.h>
+#endif
